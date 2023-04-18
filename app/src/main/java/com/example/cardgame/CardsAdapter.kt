53fc66f5d117cb3cd0a13aa81391f640f01e1263
Asocialboi <koltonlpx@gmail.com>
2023-04-18 11:04:26 +0300

    v2

--- app/src/main/java/com/example/cardgame/CardsAdapter.kt
+++ app/src/main/java/com/example/cardgame/CardsAdapter.kt
@@ -1,33 +1,28 @@
 package com.example.cardgame
+
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.cardgame.databinding.ItemCardBinding
 
-class CardsAdapter(
-    private val cards: List<Card>,
-    private val isCurrentUser: Boolean
-) : RecyclerView.Adapter<CardsAdapter.CardViewHolder>() {
+class CardsAdapter(private val cards: List<Card>) : RecyclerView.Adapter<CardsAdapter.ViewHolder>() {
 
-    class CardViewHolder(val binding: ItemCardBinding) : RecyclerView.ViewHolder(binding.root)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
-        val binding = ItemCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return CardViewHolder(binding)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cardValue: TextView = itemView.findViewById(R.id.tv_card_value)
     }
 
-    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val card = cards[position]
-
-        // Якщо isCurrentUser == true, відображайте зображення карт; інакше відображайте зображення зворотнього боку карт
-        val cardImageResId = if (isCurrentUser) {
-            card.imageResourceId
-        } else {
-            R.drawable.atlas
-        }
-
-        holder.binding.ivCard.setImageResource(cardImageResId)
+        holder.cardValue.text = card.value.toString()
     }
 
-    override fun getItemCount() = cards.size
+    override fun getItemCount(): Int {
+        return cards.size
+    }
 }
